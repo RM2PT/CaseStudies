28d4371699a43be505a07f64737cf51ad11e5f72
yylonly <yylonly@gmail.com>
2018-08-13 22:17:28 +0800

    CoCoME GUI Test

--- net.mydreamy.casestudies.cocome.autogui.refined/src-gen/gui/MainTest.java
+++ net.mydreamy.casestudies.cocome.autogui.refined/src-gen/gui/MainTest.java
@@ -1,5 +1,6 @@
 package gui;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 import java.io.IOException;
@@ -16,12 +17,15 @@ import org.testfx.matcher.control.LabeledMatchers;
 import org.testfx.matcher.control.TableViewMatchers;
 import org.testfx.matcher.control.TextMatchers;
 
+import gui.supportclass.AssociationInfo;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TitledPane;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.MouseButton;
 import javafx.application.Platform;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.TabPane;
 import javafx.scene.control.TableView;
@@ -43,14 +47,10 @@ public class MainTest extends ApplicationTest {
     
    
     @Test
+    @Ignore
     public void test() {
     	
-    	clickOn("System Status");
-    	clickOn("Load Status");
-    	type(KeyCode.DOWN);
-    	type(KeyCode.ENTER);    	
-
-    	
+   	
     }
     
     
@@ -103,6 +103,28 @@ public class MainTest extends ApplicationTest {
     	
     }
     
+   public void validateAssociation(String association, int number) {
+    	
+   		TableView tvU = (TableView)((TitledPane)lookup("#status_left_pane_association").query()).getContent();
+   		
+   		ObservableList<AssociationInfo> l =  tvU.getItems();
+   		
+   		
+   		AssociationInfo obU = null;
+   		
+   		for (AssociationInfo m : l) {
+   			
+   			if (m.getAssociationName().equals(association)) {
+   				
+   				obU = m;
+   				break;
+   			}
+   		}
+		
+		assertEquals(obU.getNumber(), number);
+    	
+    }
+    
     public void returnTrue() {
     	
     	FxAssert.verifyThat((TitledPane)lookup("#operation_return_pane").query(), NodeMatchers.isNotNull());
@@ -117,8 +139,62 @@ public class MainTest extends ApplicationTest {
 		
     }
     
+    public void returnTable(String attribute, String value, int order) {
+    	
+    	TitledPane n = (TitledPane)lookup("#operation_return_pane").query();
+    	TableView tvU =  (TableView) n.getContent();
+    	
+		Map<String, String> obU = (Map<String, String>) tvU.getItems().get(order);
+		assertEquals(obU.get(attribute), value);
+    }
+    
+    public void loadData() {
+    	
+    	clickOn("System Status");
+    	clickOn("Load Status");
+    	sleep(1000);
+    	type(KeyCode.DOWN);
+    	type(KeyCode.ENTER);
+    	
+    }
+    
+    public void saveData() {
+    	
+    	clickOn("System Status");
+    	clickOn("Save Status");
+    	sleep(2000);
+    	type(KeyCode.C);
+    	type(KeyCode.O);
+    	type(KeyCode.C);
+    	type(KeyCode.O);
+    	type(KeyCode.M);
+    	type(KeyCode.E);
+    	sleep(1000);
+    	type(KeyCode.ENTER);
+    	sleep(1000);
+    }
+    
+   public void saveDataFinal() {
+    	
+    	clickOn("System Status");
+    	clickOn("Save Status");
+    	sleep(2000);
+    	type(KeyCode.C);
+    	type(KeyCode.O);
+    	type(KeyCode.C);
+    	type(KeyCode.O);
+    	type(KeyCode.M);
+    	type(KeyCode.E);
+    	type(KeyCode.F);
+    	type(KeyCode.I);
+    	type(KeyCode.N);
+    	sleep(1000);
+    	type(KeyCode.ENTER);
+    	sleep(1000);
+    }
+    
     @Test
-    public void a_testPrepareData() {
+    public void test_PrepareData() {
     		
 			//create store UM
     		clickOn("System Function");
@@ -138,11 +214,18 @@ public class MainTest extends ApplicationTest {
     		new FxRobot().doubleClickOn("manageProductCatalog").clickOn("createProductCatalog").type(KeyCode.TAB).type(KeyCode.TAB).write("1").type(KeyCode.TAB).write("Fruits").clickOn("#execute");
     		returnTrue();
     		
-    		//create catalog
+    		//create item
     		new FxRobot().doubleClickOn("manageItem").clickOn("createItem").type(KeyCode.TAB).type(KeyCode.TAB).write("1").type(KeyCode.TAB).write("Apple").type(KeyCode.TAB).write("10").type(KeyCode.TAB).write("1000").type(KeyCode.TAB).write("6").clickOn("#execute");
     		returnTrue();
     		new FxRobot().clickOn("createItem").type(KeyCode.TAB).type(KeyCode.TAB).write("2").type(KeyCode.TAB).write("PineApple").type(KeyCode.TAB).write("30").type(KeyCode.TAB).write("1000").type(KeyCode.TAB).write("20").clickOn("#execute");
     		returnTrue();
+    		new FxRobot().clickOn("createItem").type(KeyCode.TAB).type(KeyCode.TAB).write("3").type(KeyCode.TAB).write("Peach").type(KeyCode.TAB).write("12").type(KeyCode.TAB).write("0").type(KeyCode.TAB).write("8").clickOn("#execute");
+    		returnTrue();
+    		
+    		//create supplier
+    		new FxRobot().doubleClickOn("manageSupplier").clickOn("createSupplier").type(KeyCode.TAB).type(KeyCode.TAB).write("1").type(KeyCode.TAB).write("UM").clickOn("#execute");
+    		returnTrue();
+    		
 
     		//Check value
     		clickOn("System Status");
@@ -162,42 +245,28 @@ public class MainTest extends ApplicationTest {
     		validateAttribute("Name", "CashDeskA");
     		sleep(1000);
     		
-    		type(KeyCode.DOWN);
-    		type(KeyCode.DOWN);
-    		type(KeyCode.DOWN);
-    		type(KeyCode.DOWN);
+    		type(KeyCode.DOWN, 4);
     		validateAttribute("Barcode", "1");
     		validateAttribute("Name", "Apple");
     		validateAttribute("Barcode", "2", 1);
     		validateAttribute("Name", "PineApple", 1);
+    		validateAttribute("Barcode", "3", 2);
+    		validateAttribute("Name", "Peach", 2);
     		sleep(1000);
     		
-        	clickOn("System Status");
-        	clickOn("Save Status");
-        	sleep(2000);
-        	type(KeyCode.C);
-        	type(KeyCode.O);
-        	type(KeyCode.C);
-        	type(KeyCode.O);
-        	type(KeyCode.M);
-        	type(KeyCode.E);
-        	sleep(1000);
-        	type(KeyCode.ENTER);
-        	sleep(1000);
-    		
+    		type(KeyCode.DOWN, 5);
+    		validateAttribute("Id", "1");
+    		validateAttribute("Name", "UM");
+    		sleep(1000);  	
     		
     }
     
-    @Test 
-    public void b_testProcessSale_CashPay() {
-    	
-    	//load data
-    	clickOn("System Status");
-    	clickOn("Load Status");
-    	sleep(1000);
-    	type(KeyCode.DOWN);
-    	type(KeyCode.ENTER);  
-    	
+    public void test_OpenStoreAndCashiDesk() {
+
+    	if (integrationTest == false) {
+	    	//load data
+	    	loadData();
+    	}	
     	
     	//open store
     	clickOn("System Function");
@@ -219,6 +288,37 @@ public class MainTest extends ApplicationTest {
 		type(KeyCode.DOWN);
     	validateAttribute("IsOpened", "true");
     	sleep(1500);
+    	
+    }
+    
+    @Test 
+    public void test_ProcessSale_CashPay() {
+    	
+    	if (integrationTest == false) {
+	    	//load data
+	    	loadData();
+	    	
+	    	//open store
+	    	clickOn("System Function");
+	    	new FxRobot().clickOn("#storemanager").sleep(1000).clickOn("openStore").doubleClickOn("#operation_paras").write("1").clickOn("#execute").sleep(500);
+	    	returnTrue();
+	    	clickOn("System Status");
+	    	clickOn("Store");
+	    	validateAttribute("IsOpened", "true");
+	    	sleep(1500);
+	    	
+	    	//open cashdesk
+	    	clickOn("System Function");
+	    	new FxRobot().clickOn("#cashier").sleep(1000).clickOn("openCashDesk").clickOn("#operation_paras").write("1").clickOn("#execute").sleep(500);
+	    	returnTrue();
+	    	clickOn("System Status");
+	    	type(KeyCode.TAB);
+			type(KeyCode.TAB);
+	    	type(KeyCode.DOWN);
+			type(KeyCode.DOWN);
+	    	validateAttribute("IsOpened", "true");
+	    	sleep(1500);
+    	}
     	
     	//make a new sale
 		clickOn("System Function");	
@@ -294,36 +394,34 @@ public class MainTest extends ApplicationTest {
     }
     
     @Test 
-    public void b_testProcessSale_CardPay() {
+    public void test_ProcessSale_CardPay() {
     	
-    	//load data
-    	clickOn("System Status");
-    	clickOn("Load Status");
-    	sleep(1000);
-    	type(KeyCode.DOWN);
-    	type(KeyCode.ENTER);  
-    	
-    	
-    	//open store
-    	clickOn("System Function");
-    	new FxRobot().clickOn("#storemanager").sleep(1000).clickOn("openStore").doubleClickOn("#operation_paras").write("1").clickOn("#execute").sleep(500);
-    	returnTrue();
-    	clickOn("System Status");
-    	clickOn("Store");
-    	validateAttribute("IsOpened", "true");
-    	sleep(1500);
-    	
-    	//open cashdesk
-    	clickOn("System Function");
-    	new FxRobot().clickOn("#cashier").sleep(1000).clickOn("openCashDesk").clickOn("#operation_paras").write("1").clickOn("#execute").sleep(500);
-    	returnTrue();
-    	clickOn("System Status");
-    	type(KeyCode.TAB);
-		type(KeyCode.TAB);
-    	type(KeyCode.DOWN);
-		type(KeyCode.DOWN);
-    	validateAttribute("IsOpened", "true");
-    	sleep(1500);
+    	if (integrationTest == false) {
+    		
+	    	//load data
+	    	loadData(); 
+	  	
+	    	//open store
+	    	clickOn("System Function");
+	    	new FxRobot().clickOn("#storemanager").sleep(1000).clickOn("openStore").doubleClickOn("#operation_paras").write("1").clickOn("#execute").sleep(500);
+	    	returnTrue();
+	    	clickOn("System Status");
+	    	clickOn("Store");
+	    	validateAttribute("IsOpened", "true");
+	    	sleep(1500);
+	    	
+	    	//open cashdesk
+	    	clickOn("System Function");
+	    	new FxRobot().clickOn("#cashier").sleep(1000).clickOn("openCashDesk").clickOn("#operation_paras").write("1").clickOn("#execute").sleep(500);
+	    	returnTrue();
+	    	clickOn("System Status");
+	    	type(KeyCode.TAB);
+			type(KeyCode.TAB);
+	    	type(KeyCode.DOWN);
+			type(KeyCode.DOWN);
+	    	validateAttribute("IsOpened", "true");
+	    	sleep(1500);
+    	}
     	
     	//make a new sale
 		clickOn("System Function");	
@@ -400,93 +498,108 @@ public class MainTest extends ApplicationTest {
     
     
     @Test
-    @Ignore
-    public void testWithDraw() {
+    public void test_ProductManagers() {
     	
-    		//input card with id 1
-    		clickOn("#customer").sleep(1000).doubleClickOn("withdrawCash").clickOn("inputCard").type(KeyCode.TAB).type(KeyCode.TAB).type(KeyCode.TAB).write("1").clickOn("#execute").sleep(1000);
-    		
-    		//verify return result
-        	FxAssert.verifyThat((TitledPane)lookup("#operation_return_pane").query(), NodeMatchers.isNotNull());
-    		FxAssert.verifyThat(((TitledPane)lookup("#operation_return_pane").query()).getContent(), NodeMatchers.hasText("true"));
-    		
-    		//input password 123456
-    		clickOn("inputPassword").type(KeyCode.TAB).type(KeyCode.TAB).type(KeyCode.TAB).write("123456").clickOn("#execute").sleep(1000);
-    		
-    		//verify return result
-        	FxAssert.verifyThat((TitledPane)lookup("#operation_return_pane").query(), NodeMatchers.isNotNull());
-    		FxAssert.verifyThat(((TitledPane)lookup("#operation_return_pane").query()).getContent(), NodeMatchers.hasText("true"));
-    		
-    		//withdraw 2000
-    		clickOn("withDraw").type(KeyCode.TAB).type(KeyCode.TAB).type(KeyCode.TAB).write("2000").clickOn("#execute").sleep(1000);
-    		
-    		//verify return result
-        	FxAssert.verifyThat((TitledPane)lookup("#operation_return_pane").query(), NodeMatchers.isNotNull());
-    		FxAssert.verifyThat(((TitledPane)lookup("#operation_return_pane").query()).getContent(), NodeMatchers.hasText("true"));
-    		
-    		//check system status of balances
-    		clickOn("System Status").clickOn("BankCard").sleep(1000);
-    		
-    		//print recipt
-    		clickOn("System Function").clickOn("printReceipt").clickOn("#execute").sleep(1000);
-     	
-    		//verify return result
-        	FxAssert.verifyThat((TitledPane)lookup("#operation_return_pane").query(), NodeMatchers.isNotNull());
-    		FxAssert.verifyThat(((TitledPane)lookup("#operation_return_pane").query()).getContent(), NodeMatchers.hasText("2000.0"));
-    		
-    		//print recipt
-    		clickOn("ejectCard").clickOn("#execute").sleep(1000);
-     	
-    		//verify return result
-        	FxAssert.verifyThat((TitledPane)lookup("#operation_return_pane").query(), NodeMatchers.isNotNull());
-    		FxAssert.verifyThat(((TitledPane)lookup("#operation_return_pane").query()).getContent(), NodeMatchers.hasText("true"));
-    		
+    	if (integrationTest == false) {
+	    	//load data
+	    	loadData();  
+    	}
+    	//stock reports
+    	clickOn("System Function");
+    	new FxRobot().clickOn("#storemanager").sleep(1000).clickOn("showStockReports").clickOn("#execute").sleep(1000);
+    	returnTable("StockNumber", "0", 2);
+    	
+    	//list suppliers
+    	new FxRobot().clickOn("listSuppliers").clickOn("#execute").sleep(1000);
+    	returnTable("Name", "UM", 0);
+    	
+    	//make new order for peach
+    	new FxRobot().doubleClickOn("orderProducts").clickOn("makeNewOrder").doubleClickOn("#operation_paras").write("1").clickOn("#execute").sleep(1000);
+    	returnTrue();
+    	
+    	//check order
+    	clickOn("System Status");
+    	clickOn("OrderProduct");
+    	validateAttribute("OrderStatus", "NEW");
+    	sleep(1000);
+    	
+    	//list all out of stock products
+    	clickOn("System Function");
+    	new FxRobot().clickOn("listAllOutOfStoreProducts").clickOn("#execute").sleep(1000);
+    	returnTable("StockNumber", "0", 0);
+    	
+    	//orderItem
+    	new FxRobot().clickOn("orderItem").doubleClickOn("#operation_paras").write("3").type(KeyCode.TAB).write("1000").clickOn("#execute").sleep(1000);
+    	returnTrue();
+    	
+    	//check order item
+    	clickOn("System Status");
+    	type(KeyCode.TAB, 2);
+    	type(KeyCode.UP, 2);
+    	validateAttribute("Quantity", "1000");
+    	validateAttribute("SubAmount", "8000.0");
+    	sleep(1000);
+    	
+    	//choose supplier
+    	clickOn("System Function");
+    	new FxRobot().clickOn("chooseSupplier").doubleClickOn("#operation_paras").write("1").clickOn("#execute").sleep(1000);
+    	returnTrue();
+    
+    	clickOn("System Status");
+    	type(KeyCode.TAB, 2);
+    	type(KeyCode.DOWN, 2);
+    	validateAssociation("Supplier", 1);
+    	sleep(1000);
+    	
+    	//place order
+    	clickOn("System Function");
+    	new FxRobot().clickOn("placeOrder").clickOn("#execute").sleep(1000);
+    	returnTrue();
+    	
+    	clickOn("System Status");
+    	type(KeyCode.TAB, 2);
+    	type(KeyCode.UP, 1);
+    	type(KeyCode.DOWN, 1);
+    	validateAttribute("OrderStatus", "REQUESTED");
+    	sleep(1000);
+    	
+    	//receive order
+    	clickOn("System Function");
+    	new FxRobot().clickOn("receiveOrderedProduct").doubleClickOn("#operation_paras").write("1").clickOn("#execute").sleep(1000);
+    	returnTrue();
+    	
+    	clickOn("System Status");
+    	type(KeyCode.TAB, 2);
+    	type(KeyCode.UP, 1);
+    	type(KeyCode.DOWN, 1);
+    	validateAttribute("OrderStatus", "RECEIVED");
+    	sleep(1000);
+    	
+    	//stock reports
+    	clickOn("System Function");
+    	new FxRobot().clickOn("#storemanager").sleep(1000).clickOn("showStockReports").clickOn("#execute").sleep(1000);
+    	returnTable("StockNumber", "1000", 2);
+    	sleep(1000);
     	
     }
     
     @Test
-    @Ignore
-    public void testDeposit() {
+    public void integrationTest() {
     	
-    		//input card with id 1
-    		doubleClickOn("depositFunds").clickOn("inputCard").type(KeyCode.TAB).type(KeyCode.TAB).type(KeyCode.TAB).write("1").clickOn("#execute").sleep(1000);
-    		
-    		//verify return result
-        	FxAssert.verifyThat((TitledPane)lookup("#operation_return_pane").query(), NodeMatchers.isNotNull());
-    		FxAssert.verifyThat(((TitledPane)lookup("#operation_return_pane").query()).getContent(), NodeMatchers.hasText("true"));
-    		
-    		//input password 123456
-    		clickOn("inputPassword").type(KeyCode.TAB).type(KeyCode.TAB).type(KeyCode.TAB).write("123456").clickOn("#execute").sleep(1000);
-    		
-    		//verify return result
-        	FxAssert.verifyThat((TitledPane)lookup("#operation_return_pane").query(), NodeMatchers.isNotNull());
-    		FxAssert.verifyThat(((TitledPane)lookup("#operation_return_pane").query()).getContent(), NodeMatchers.hasText("true"));
-    		
-    		//withdraw 2000
-    		clickOn("deposit").type(KeyCode.TAB).type(KeyCode.TAB).type(KeyCode.TAB).write("2000").clickOn("#execute").sleep(1000);
-    		
-    		//verify return result
-        	FxAssert.verifyThat((TitledPane)lookup("#operation_return_pane").query(), NodeMatchers.isNotNull());
-    		FxAssert.verifyThat(((TitledPane)lookup("#operation_return_pane").query()).getContent(), NodeMatchers.hasText("true"));
-    		
-    		//check system status of balances
-    		clickOn("System Status").clickOn("BankCard").sleep(1000);
-    		
-    		//print recipt
-    		clickOn("System Function").clickOn("printReceipt").clickOn("#execute").sleep(1000);
-     	
-    		//verify return result
-        	FxAssert.verifyThat((TitledPane)lookup("#operation_return_pane").query(), NodeMatchers.isNotNull());
-    		FxAssert.verifyThat(((TitledPane)lookup("#operation_return_pane").query()).getContent(), NodeMatchers.hasText("2000.0"));
-    		
-    		//print recipt
-    		clickOn("ejectCard").clickOn("#execute").sleep(1000);
-     	
-    		//verify return result
-        	FxAssert.verifyThat((TitledPane)lookup("#operation_return_pane").query(), NodeMatchers.isNotNull());
-    		FxAssert.verifyThat(((TitledPane)lookup("#operation_return_pane").query()).getContent(), NodeMatchers.hasText("true"));
-    		
-    	    
-    		
+    	integrationTest = true;
+    	
+    	test_PrepareData();
+    	saveData();
+    	loadData();
+    	
+    	test_OpenStoreAndCashiDesk();
+    	test_ProcessSale_CashPay();
+    	test_ProductManagers();
+    	
+    	saveDataFinal();
+    	
     }
+    
+    private boolean integrationTest;
+    
 }
